273da56be57725269dbeb0fca65f7aca28fd5949
Seu Nome <seuemail@exemplo.com>
2024-12-11 13:57:14 -0300

    JUinit teste do sistema

---
+++ src/test/java/com/generation/blogpeassoal/controller/UsuarioControllerTest.java
@@ -0,0 +1,32 @@
+package com.generation.blogpeassoal.controller;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import com.generation.blogpessoal.repository.UsuarioRepository;
+import com.generation.blogpessoal.service.UsuarioService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsuarioControllerTest {
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@BeforeAll
+	void start() {
+		usuarioRepository.deleteAll();
+		
+		usuarioService.cadastrarUsuario(new usuario());
+		
+		
+	}
+}
